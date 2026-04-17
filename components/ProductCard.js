8bd5ee541644b82c5d50547261c4732e92a76f14
diegoTorres218 <torresdiego218@gmail.com>
2026-04-17 15:44:09 -0400

    Update ProductCard.js

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,1 +1,10 @@
+export const ProductCard = ({ product, onAddToCart }) => {
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+    onAddToCart(product);
+  };
 
+  return (
+    // ... JSX igual
+  );
+};
