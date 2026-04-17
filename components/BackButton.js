20bcfea573d054ca67fc2c2165851cbf29dfc73a
diegoTorres218 <torresdiego218@gmail.com>
2026-04-17 15:43:20 -0400

    Update BackButton.js

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,1 +1,13 @@
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
+export default function BackButton({ navigation }) {
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.goBack()}
+      style={{ padding: 10 }}
+    >
+      <Ionicons name="arrow-back" size={28} color="#FF6B00" />
+    </TouchableOpacity>
+  );
+}
